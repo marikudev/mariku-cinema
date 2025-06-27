71861be8e700a2a750aaa65d17b847cb7c70671d
marikudev <malik.workfile@gmail.com>
2025-06-27 10:15:57 +0900

    add next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: [
+      "image.tmdb.org",
+      "upload.wikimedia.org",
+    ],
+  },
 };
 
 export default nextConfig;
